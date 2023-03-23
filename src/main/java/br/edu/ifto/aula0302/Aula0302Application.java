eb67c4596804f7818af38682810eefb1ca789c14
Charles Albert <charles.ccomp@gmail.com>
2023-03-23 10:58:39 -0300

    commit inicial, estrutura basica de projeto

---
+++ src/main/java/br/edu/ifto/aula0302/Aula0302Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.aula0302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula0302Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula0302Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifto/aula0302/Aula0302ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.aula0302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula0302ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
